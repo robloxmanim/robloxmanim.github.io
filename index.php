aad9d5c1084a510367be0b191d77b963d2ec8021
robloxmanim <learningrobloxwhatevertheheck@gmail.com>
2025-06-16 19:19:57 -0700

    git commit -m "This is crazy"

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <!-- META data stuff -->
+    <head>
+
+    </head>
+    <!-- Actual Visual HTML -->
+    <body>
+
+    </body>
+    <!-- JavaScript -->
+    <script>
+
+    </script>
+    <!-- Cascading Style Sheets -->
+    <style>
+
+    </style>
+    <!-- PHP HyperText Processor -->
+</html>
